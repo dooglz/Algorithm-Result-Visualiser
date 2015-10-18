0c40aaed53bd1c72bae20f783baa3f29fb4d8a49
Sam Serrels <doogly1@gmail.com>
2015-10-18 04:01:32 +0100

    reading and displaying real data

--- js/chart.js
+++ js/chart.js
@@ -4,8 +4,8 @@ Chart.Construct = function (chartDiv) {
   Chart.chartDiv  = d3.select("#chartDiv");
   Chart.$chartDiv  = $("#chartDiv");
   Chart.species = ["setosa", "versicolor", "virginica"],
-  Chart.paddingTop = 80;
-  Chart.paddingBottom = 80;
+  Chart.paddingTop = 20;
+  Chart.paddingBottom = 20;
   Chart.paddingRight = 100;
   Chart.paddingLeft= 100;
   Chart.width = Chart.$chartDiv.width() -  Chart.paddingRight - Chart.paddingLeft;
@@ -14,6 +14,8 @@ Chart.Construct = function (chartDiv) {
   Chart.yScale = {};
   Chart.line = d3.svg.line();
   Chart.axis = d3.svg.axis().orient("left");
+  Chart.axisMid = d3.svg.axis().orient("left").ticks(0);
+  Chart.axisEnd = d3.svg.axis().orient("right");
   Chart.foreground = {};
   Chart.svg = Chart.chartDiv
     .append("svg:svg")
@@ -47,15 +49,16 @@ Chart.ChangeData = function (data) {
   Chart.yScale = {};
   console.log(data);
   // Create a scale and brush for each event.
+  var a = data.eventTimes;
+  
+  var globalExtent = d3.extent(Flatten2D(a));
   for (var i = 0; i < data.events.length; i++) {
     var e = data.events[i];
     Chart.yScale[e] = d3.scale.linear()
-      .domain(d3.extent(data.eventTimes, function (p) { return p[i]; }))
-      .range([Chart.height, 0]);
-
-    Chart.yScale[e].brush = d3.svg.brush()
-      .y(Chart.yScale[e])
-      .on("brush", Chart.brush);
+      //.domain(d3.extent(data.eventTimes[i]))
+      .domain(globalExtent)
+      .range([Chart.height, 0])
+      .clamp(true);
   }
 
   // Add foreground lines.
@@ -73,16 +76,19 @@ Chart.ChangeData = function (data) {
     .enter().append("svg:g")
     .attr("class", "event")
     .attr("transform", function (d) { return "translate(" + Chart.xScale(d) + ")"; })
-    .call(d3.behavior.drag()
-      .origin(function (d) { return { x: Chart.xScale(d) }; })
-      .on("dragstart", dragstart)
-      .on("drag", drag)
-      .on("dragend", dragend));
-
+    
   // Add an axis and title.
   var axises =  g.append("svg:g")
     .attr("class", "axis")
-    .each(function (d) { d3.select(this).call(Chart.axis.scale(Chart.yScale[d])); });
+    .each(function (d,i) {
+      if(i ==0 ){
+      d3.select(this).call(Chart.axis.scale(Chart.yScale[d])); 
+          }else if(i == Chart.Events.length-1){
+             d3.select(this).call(Chart.axisEnd.scale(Chart.yScale[d])); 
+      }else{
+         d3.select(this).call(Chart.axisMid.scale(Chart.yScale[d])); 
+      }
+     });
  
     axises.append("svg:text")
     .attr("text-anchor", "middle")
@@ -92,32 +98,6 @@ Chart.ChangeData = function (data) {
     .attr("text-anchor", "middle")
     .attr("y", Chart.height+20)
     .text(String);
-  // Add a brush for each axis.
-  /*
-  g.append("svg:g")
-    .attr("class", "brush")
-    .each(function (d) { d3.select(this).call(Chart.yScale[d].brush); })
-    .selectAll("rect")
-    .attr("x", -8)
-    .attr("width", 16);
-    */
-  function dragstart(d) {
-    i = Chart.Events.indexOf(d);
-  }
-
-  function drag(d) {
-    x.range()[i] = d3.event.x;
-    Chart.Events.sort(function (a, b) { return x(a) - x(b); });
-    g.attr("transform", function (d) { return "translate(" + x(d) + ")"; });
-    Chart.foreground.attr("d", Chart.path);
-  }
-
-  function dragend(d) {
-    x.domain(Chart.Events).rangePoints([0, w]);
-    var t = d3.transition().duration(500);
-    t.selectAll(".event").attr("transform", function (d) { return "translate(" + x(d) + ")"; });
-    t.selectAll(".foreground path").attr("d", path);
-  }
 }
 
 
@@ -126,15 +106,9 @@ Chart.path = function (d) {
   return Chart.line(Chart.Events.map(function (p) { return [Chart.xScale(p), Chart.yScale[p](d[p])]; }));
 }
 
-// Handles a brush event, toggling the display of foreground lines.
-Chart.brush = function () {
-  var actives = Chart.Events.filter(function (p) { return !Chart.yScale[p].brush.empty(); });
-  var extents = actives.map(function (p) { return Chart.yScale[p].brush.extent(); });
-  Chart.foreground.classed("fade", function (d) {
-    return !actives.every(function (p, i) {
-      return extents[i][0] <= d[p] && d[p] <= extents[i][1];
-    });
-  });
-}
+//Chart.ChangeData(DataParser.Combine([sample1,sample2,sample3]));
 
-Chart.ChangeData(DataParser.Combine([sample1,sample2,sample3]));
+var csv1;
+var csv2;
+d3.text("test.csv", function(d){csv1 = DataParser.ParseCSV(d);});
+d3.text("test2.csv", function(d){csv2 = DataParser.ParseCSV(d);});
--- js/dataParser.js
+++ js/dataParser.js
@@ -3,29 +3,56 @@ var DataParser = DataParser || {};
 
 var sample1 = {
   tests: ["sample1"],
-  eventTimes: [0,5,10],
-  events: ["Start","Middle","End"],
+  eventTimes: [0, 5, 10],
+  events: ["Start", "Middle", "End"],
 }
 var sample2 = {
   tests: ["sample2"],
-  eventTimes: [0,2,10],
-  events: ["Start","Middle","End"],
+  eventTimes: [0, 2, 10],
+  events: ["Start", "Middle", "End"],
 }
 var sample3 = {
   tests: ["sample3"],
-  eventTimes: [0,6,10],
-  events: ["Start","Middle","End"],
+  eventTimes: [0, 6, 10],
+  events: ["Start", "Middle", "End"],
 }
 
-DataParser.ParseCSV = function (rawCSV) {
-  var obj = {};
-  obj.tests = [];
-  obj.events = [];
-  obj.events.push("Start");
-  obj.events.push("Middle");
-  obj.events.push("End");
-  obj.eventTimes = [[0],[5],[10]];
-  return obj;
+DataParser.ParseCSV = function (csv) {
+  var ParseHeadder = function (headder) {
+    for (var i = 0; i < headder.length; i++) {
+      headder[i] = Sanitise(headder[i]);
+      if(IsNumber(headder[i])){
+        headder[i] = +headder[i];
+      }
+    }
+    if (Empty(headder[headder.length - 1])) {
+      headder.pop();
+    }
+    return headder;
+  };
+  
+  var lines = csv.split("\n");
+  var result = {};
+  var i;
+  for (i = 0; i < lines.length; i++) {
+    var headers = ParseHeadder(lines[i].split(","));
+    if (headers.length == 0) {
+      continue;
+    } else if (headers.length == 1) {
+      console.warn("Parsing csv, unkown line: ", lines);
+    } else if (headers[0] == "Run") {
+      break;
+    } else if (headers.length == 2) {
+      result[Sanitise(headers[0])] = Sanitise(headers[1]);
+    }
+  }
+  result.tests = [result.name];
+  result.events = ParseHeadder(lines[i].split(","));
+  result.events[0] = "Start";
+  result.eventTimes = ParseHeadder(lines[i + 1].split(","));
+  result.eventTimes[0] = 0;
+  cumulative(result.eventTimes);
+  return result;
 }
 
 DataParser.Combine = function (testData) {
@@ -39,17 +66,18 @@ DataParser.Combine = function (testData) {
   obj.d3data = [];
   for (var i = 0; i < testData.length; i++) {
     obj.tests = obj.tests.concat(testData[i].tests);
-    Horizontal2DMerge(obj.eventTimes,testData[i].eventTimes);
-    
-    var d3d  = {name:testData[i].tests[0]};
+    Horizontal2DMerge(obj.eventTimes, testData[i].eventTimes);
+
+    var d3d = { name: testData[i].tests[0] };
     //verify events
     for (var j = 0; j < testData[i].events.length; j++) {
       if ($.inArray(testData[i].events[j], obj.events) != j) {
         console.error("comparing Tests with different events!");
+        return {};
       }
       d3d[testData[i].events[j]] = testData[i].eventTimes[j];
     }
-     obj.d3data.push(d3d);
+    obj.d3data.push(d3d);
   }
   return obj;
 }

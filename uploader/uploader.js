042dbfe5ec3dc145d253ca607a5fc3f040e5c594
Sam Serrels <doogly1@gmail.com>
2015-10-19 18:01:31 +0100

    uploader work

--- uploader/uploader.js
+++ uploader/uploader.js
@@ -2,14 +2,31 @@ var request = require('request');
 var fs = require("fs");
 var url = "http://vps.samserrels.com/res/upload.php"
 
-var formData = {
-  // Pass data via Streams
-  upfile: fs.createReadStream(__dirname + '/Sequential_LinPack200_Sat_Oct_17_15-58-10_2015.csv')
-};
-request.post({url:url, formData: formData}, function optionalCallback(err, httpResponse, body) {
-	 console.log('resp: ' + httpResponse);
-  if (err) {
-    return console.error('upload failed:', err);
-  }
-  console.log('Upload successful!  Server responded with:', body);
-});
+var file = process.argv[2];
+
+if(file !== undefined && file != ""){
+	console.log("Attempting to upload: ",file);
+	var fileStream = fs.createReadStream(file);
+	var formData = {
+		  upfile: fileStream
+	};
+	
+	fileStream.on('open', function () {
+    	request.post({url:url, formData: formData}, function optionalCallback(err, httpResponse, body) {
+		console.log('resp: ' + httpResponse);
+		if (err) {
+			return console.error('upload failed:', err);
+		}
+		console.log('Upload successful!  Server responded with:', body);
+	});
+	
+	fileStream.on('error', function(err) {
+	console.log("File open fail ",err);
+	});
+	
+  });
+	
+}else{
+	console.log("Please specify a file");
+}
+

49adb0ecc2493e68dcaabf7d485207a714f8e0f2
Sam Serrels <doogly1@gmail.com>
2015-11-10 18:06:26 +0000

    Files.php

---
+++ files.php
@@ -0,0 +1,28 @@
+<?php
+	$files = array_map('basename', glob("./uploads/*.{json,csv}", GLOB_BRACE));
+	$allfileObjs = [];
+	for ($i=0; $i < sizeof($files); $i++) { 
+		//echo "<br>File ".$files[$i]."<br>";
+		$file = fopen("uploads/".$files[$i],"r");
+		$j = 0;
+		$lineObj = [];
+		$lineObj["filename"] = $files[$i];
+		while(! feof($file))
+		{
+			$line = fgetcsv($file);
+			if($line[0] == "0"){
+				break;
+			}
+			if(sizeof($line) == 1){
+				$lineObj[$line[0]] = "";
+			}else if(sizeof($line) == 2){
+				$lineObj[$line[0]] = $line[1];
+			}else{
+				$lineObj[$line[0]] = array_slice($line, 1);
+			}
+		}
+		fclose($file);
+		array_push($allfileObjs,$lineObj);
+	}
+	echo json_encode($allfileObjs);
+?>

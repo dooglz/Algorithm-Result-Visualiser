fcd9a454a194a3e6bf9329c0667e30e1c5d18ab3
Sam Serrels <doogly1@gmail.com>
2015-11-10 18:54:40 +0000

    Datatables.

--- js/main.js
+++ js/main.js
@@ -1,79 +1,70 @@
-
-var t = $("#transferDiv").bootstrapTransfer(
-				{'target_id': 'multi-select-input',
-				 'height': '15em',
-				 'hilite_selection': true
-				 }
-		);
-	
-var index = [];	
-var jqxhr = $.getJSON( "http://vps.samserrels.com/res/dir.php", function( data ) {				 
-	console.log(data);
-	index = data;
-	Populate()
-});		 
-jqxhr.fail(function() {
-    console.log( "error" );
-	index = ["GpuParrallelSort65536_Wed_Oct_21_15-20-49_2015.csv","GpuParrallelSort256_Mon_Oct_19_16-57-05_2015.csv","GpuParrallelSort65536_Wed_Oct_21_15-17-56_2015.csv","GpuParrallelSort256_Wed_Oct_21_15-12-46_2015.csv","GpuParrallelSort256_Mon_Oct_19_16-57-16_2015.csv","Sequential_LinPack10_Sat_Oct_17_18-44-11_2015.csv","Sequential_LinPack10_Sat_Oct_17_18-45-13_2015.csv","Sequential_LinPack200_Sat_Oct_17_15-58-10_2015.csv","sort_2015-10-07_11-01-51.csv","sort_2015-10-07_11-02-00.csv","sort_2015-10-07_11-13-15.csv","sort_2015-10-12_15-18-27.csv"]
-	Populate();
-});
-
+//var server = "http://vps.samserrels.com/res/";
+var server = "http://localhost/";
+var index = [];
+var fileData = [];
+var tableData = [];
+var tableCollumns = [];
 var indexObjects = [];
 
-function Populate(){
+function Exists(a) {
+	return (a !== undefined && a !== null && a !== "");
+}
+
+function Populate() {
+	return;
 	var populus = [];
-	index.forEach(function(element) {
+	index.forEach(function (element) {
 		var o = DataParser.DataNameToObj(element);
 		indexObjects.push(o);
-		populus.push({value:o.id, content:o.name});
+		populus.push({ value: o.id, content: o.name });
 	}, this);
 	t.populate(populus);
 }
 
-function AddToChart(name){
-	console.log("Adding",name);
-	if(!Exists(name)){
+function AddToChart(name) {
+	console.log("Adding", name);
+	if (!Exists(name)) {
 		//scan manually
 	}
 	//first find the indexobject
 	var io = $.grep(indexObjects, function (a) { return name === a.id; });
-	if (io.length != 1){console.error("Uh OH",name); return;}
+	if (io.length != 1) { console.error("Uh OH", name); return; }
 	io = io[0];
 	//is it loaded?
-	if(Exists(io.csv)){
+	if (Exists(io.csv)) {
 		//woop
-		if(!Exists(io.parsed)){
+		if (!Exists(io.parsed)) {
 			io.parsed = DataParser.ParseCSV(io.csv);
 			console.log(io.parsed);
 		}
 		//compatable?
-		if(inChart.length > 0 && !DataParser.MatchingEvents(inChart[0].parsed,io.parsed)){
+		if (inChart.length > 0 && !DataParser.MatchingEvents(inChart[0].parsed, io.parsed)) {
 			//incompatable
 			console.warn("incompatable");
 			t.un_select(name);
-		}else{
+		} else {
 			console.warn("added");
 			inChart.push(io);
 			UpdateChart();
 		}
-	}else{
+	} else {
 		//loadin
-		var csvjqxhr = $.get( "uploads/"+io.id, function( data ) {				 
+		var csvjqxhr = $.get("uploads/" + io.id, function (data) {
 			console.log("Data load success" + io.id);
 			io.csv = data;
 			AddToChart(name);
-		});	
-		csvjqxhr.fail(function(e) {
-    		console.log( "error",e );	
+		});
+		csvjqxhr.fail(function (e) {
+			console.log("error", e);
 		});
 	}
-	
+
 }
 
 var inChart = [];
-function UpdateChart(){
+function UpdateChart() {
 	var a = [];
-	inChart.forEach(function(e) {
+	inChart.forEach(function (e) {
 		a.push(e.parsed)
 	}, this);
 	Chart.ChangeData(DataParser.Combine(a));
@@ -95,11 +86,115 @@ function RemoveFromChart(name) {
 	UpdateChart();
 }
 
-t.on( "added", function(e,name) {
-  AddToChart(name);
-});
+//----
 
-t.on( "removed", function(e,name) {
-  console.log("Removing",name);
-  RemoveFromChart(name);
-});
+
+var tdiv;
+var table;
+
+function Reorder(a,b) {
+	var ai = -1;
+	var bi = -1;
+	for (var i = 0; i < tableCollumns.length; i++) {
+		if(tableCollumns[i].title == a){ai = i;}
+		if(tableCollumns[i].title == b){bi = i;}
+		if(ai != -1 && bi != -1){
+			break;
+		}
+	}
+	if(ai == -1 || bi == -1){
+		console.error(ai,bi);
+		return false;
+	}
+	var t = tableCollumns[ai];
+	tableCollumns[ai] = tableCollumns[bi];
+	tableCollumns[bi] = t;
+	for (var i = 0; i < tableData.length; i++) {
+		var t = tableData[i][ai];
+		tableData[i][ai] = tableData[i][bi];
+		tableData[i][bi] = t;
+	}
+}
+
+
+function InitTable() {
+	if (!Exists(fileData)) { return; }
+	tableData = [];
+	tableCollumns = [];	
+	
+	//get collums
+	var keys = Object.keys(fileData[0]);
+	keys.forEach(function (k) {
+		if (!$.isArray(fileData[0][k])) {
+			tableCollumns.push({ title: k });
+			for (var j = 0; j < fileData.length; j++) {
+				if (tableData.length == j) { tableData.push([]); }
+				if (!Exists(fileData[j][k])) {
+					fileData[j][k] = "-";
+				}
+				tableData[j].push(fileData[j][k])
+			}
+
+		}
+	}, this);
+	Reorder("filename","name");
+	table.html("");
+	table.DataTable({
+		data: tableData,
+		scrollX: true,
+		columns: tableCollumns,
+		select: true,
+		columnDefs: [
+			{
+                "targets": [ 1 ],
+                "width": "10px"
+            },
+            {
+                "targets": [ 4 ],
+                "visible": false,
+            },
+            {
+                "targets": [ 5 ],
+                "visible": false
+            },
+			            {
+                "targets": [6 ],
+                "visible": false
+            },
+			            {
+                "targets": [ 7 ],
+                "visible": false
+            },
+			            {
+                "targets": [ 8 ],
+                "visible": false
+            },     {
+                "targets": [ 9 ],
+                "visible": false
+            },     {
+                "targets": [ 10 ],
+                "visible": false
+            },     {
+                "targets": [ 11 ],
+                "visible": false
+            }
+        ]
+	});
+}
+
+
+function GetFiles() {
+	var jqxhr = $.getJSON(server + "files.php", function (data) {
+		fileData = data;
+		InitTable();
+	});
+	jqxhr.fail(function (e) {
+		console.log("error", e);
+	});
+}
+
+$(document).ready(function () {
+	tdiv = $("#tableDiv");
+	table = $('#table_id');
+})
+;
